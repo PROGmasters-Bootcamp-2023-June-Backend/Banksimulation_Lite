6fbe92a33b82a4158d40c76d5e582c9a277d8a6d
fpat96 <scann.fpat@gmail.com>
2023-06-27 22:51:51 +0200

    bank finished

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -5,11 +5,23 @@ public class BankAccount {
     private String accountNumber;
     private String ownerName;
     private double balance;
-
     public BankAccount(String accountNumber, String ownerName) {
         this.accountNumber = accountNumber;
         this.ownerName = ownerName;
         this.balance = 0;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (balance - amount > 0) {
+            this.balance -= amount;
+        }else {
+            System.out.println("Account's balance too low!");
+        }
+
     }
 
     public String getAccountNumber() {
@@ -36,18 +48,6 @@ public class BankAccount {
         this.balance = balance;
     }
 
-    public void deposit(double amount) {
-        this.balance += amount;
-    }
-
-    public void withdraw(double amount) {
-        if (balance - amount > 0) {
-            this.balance -= amount;
-        }else {
-            System.out.println("not enough money");
-        }
-
-    }
     @Override
     public String toString() {
         return "BankAccount{" +
--- src/main/java/org/example/BankAccountManager.java
+++ src/main/java/org/example/BankAccountManager.java
@@ -1,5 +1,70 @@
 package org.example;
 
-public class BankAccountManager {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BankAccountManager {
+    private List<BankAccount> accounts = new ArrayList<>();
+
+    public BankAccountManager() {
+
+    }
+
+    public BankAccount findAccount(String accountNumber) {
+        for (BankAccount account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public void createAccount(String ownerName, String accountNumber) {
+        if (findAccount(accountNumber) != null) {
+            System.out.println("Account number already taken");
+        } else {
+            BankAccount bankAccount = new BankAccount(accountNumber, ownerName);
+            accounts.add(bankAccount);
+        }
+
+    }
+
+    public void deposit(String accountNumber, double amount) {
+        BankAccount bankAccountFound = findAccount(accountNumber);
+        if (findAccount(accountNumber) != null) {
+            bankAccountFound.deposit(amount);
+        }
+
+    }
+
+    public String withdrawFromAccount(String accountNumber, double amount) {
+        BankAccount bankAccountFound = findAccount(accountNumber);
+        if (findAccount(accountNumber) != null&&findAccount(accountNumber).getBalance()-amount>0) {
+            bankAccountFound.withdraw(amount);
+            return "Amount withdraw successfully";
+        } else if (findAccount(accountNumber) != null &&findAccount(accountNumber).getBalance()-amount<0 ) {
+        return "Account's balance too low!";
+        } else {
+            return "Account not found";
+        }
+    }
+    public double getAccountBalance(String accountNumber){
+        BankAccount bankAccountFound = findAccount(accountNumber);
+        if (findAccount(accountNumber) != null) {
+            return bankAccountFound.getBalance();
+        }
+        else {
+            return -1;
+        }
+    }
+
+    public List<BankAccount> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<BankAccount> accounts) {
+        this.accounts = accounts;
+    }
 }
+
+
