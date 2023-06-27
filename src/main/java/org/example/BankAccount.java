0af14f3a3157e799a46c1503098c99613592c06e
Wendycica <k.petraaa@gmail.com>
2023-06-27 17:46:38 +0200

    Bank Account 06.27

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,4 +1,39 @@
 package org.example;
 
+import java.util.PrimitiveIterator;
+
 public class BankAccount {
+
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String ownerName) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = 0.0;
+    }
 }
