5964db31f45807bac10f0472be0b82cbda7191b2
Lenovo <angyalgellert3@gmail.com>
2023-06-27 18:37:37 +0200

    Breakroom 5, Bank Account exercise solved

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,4 +1,69 @@
 package org.example;
 
 public class BankAccount {
+
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String ownerName) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = 0;
+    }
+
+    public void deposit(double amount) {
+        if (amount < 0) {
+            System.out.println("Amount must be positive");
+            return;
+        }
+        this.balance = this.balance + amount;
+    }
+
+    public String withdraw(double amount) {
+        if (amount < 0) {
+            return "Amount must be positive";
+
+        }
+
+        if (amount > balance) {
+            return "Account's balance too low!";
+
+        }
+        this.balance = this.balance - amount;
+        return "Amount withdraw successfully";
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountNumber='" + accountNumber + '\'' +
+                ", ownerName='" + ownerName + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
---
+++ src/test/java/org/example/BankAccountManager.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class BankAccountManager {
+
+    private List<BankAccount> bankAccounts;
+
+    public BankAccountManager() {
+        this.bankAccounts = new ArrayList<>();
+    }
+
+    public BankAccount findAccount(String accountNumber) {
+
+        for (BankAccount bankAccount : bankAccounts) {
+            if (bankAccount.getAccountNumber().equals(accountNumber)) {
+                return bankAccount;
+            }
+        }
+        return null;
+    }
+
+
+    public void createAccount(String ownerName, String accountNumber) {
+        if (findAccount(accountNumber) != null) {
+            return;
+        }
+
+        BankAccount bankAccount = new BankAccount(accountNumber, ownerName);
+        bankAccounts.add(bankAccount);
+    }
+
+    public String deposit(String accountNumber, double amount) {
+        BankAccount bankAccount = findAccount(accountNumber);
+        if (bankAccount == null) {
+            return "Account not found";
+        }
+        bankAccount.deposit(amount);
+        return "Amount deposit successfully";
+    }
+
+    public String withdrawFromAccount(String accountNumber, double amount) {
+        BankAccount bankAccount = findAccount(accountNumber);
+        if (bankAccount == null) {
+
+            return "Account not found";
+        }
+        return bankAccount.withdraw(amount);
+
+    }
+
+    public double getAccountBalance(String accountNumber) {
+        BankAccount bankAccount = findAccount(accountNumber);
+        if (bankAccount == null) {
+            return -1;
+        }
+        return bankAccount.getBalance();
+    }
+
+    public List<BankAccount> getAccounts() {
+        return bankAccounts;
+    }
+}
+
+
