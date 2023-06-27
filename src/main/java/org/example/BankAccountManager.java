f09af95772eddd41cb67841d316173d01fcdfa75
JanosHrutka <janos.hrutka3@gmail.com>
2023-06-27 19:57:03 +0200

    Update findAccount methods

--- src/main/java/org/example/BankAccountManager.java
+++ src/main/java/org/example/BankAccountManager.java
@@ -6,23 +6,24 @@ import java.util.List;
 public class BankAccountManager {
 
     private List<BankAccount> accounts;
-    private boolean readyAccount = false;
+    private   boolean readyAccount;
 
 
     public BankAccountManager() {
         this.accounts = new ArrayList<>();
+        this.readyAccount = false;
     }
 
     public void createAccount(String ownerName, String accountNumber) {
-        findAccount(accountNumber);
-        if(!readyAccount){
-        BankAccount bankAccountAdd = new BankAccount(accountNumber, ownerName);
-        accounts.add(bankAccountAdd);
-        }
+        findAccountBoolean(accountNumber);
+        if (!readyAccount) {
+                BankAccount bankAccountAdd = new BankAccount(accountNumber, ownerName);
+                accounts.add(bankAccountAdd);
+            }
 
     }
 
-    public boolean findAccount(String accountNumber) {
+    public boolean findAccountBoolean(String accountNumber) {
         for (BankAccount bankAccount : accounts) {
             if (bankAccount.getAccountNumber().equals(accountNumber)) {
                 readyAccount = true;
@@ -33,21 +34,36 @@ public class BankAccountManager {
         return readyAccount;
     }
 
-    public void deposit(String accountNumber, double amount) {
+
+    public BankAccount findAccount(String accountNumber) {
+        for (BankAccount bankAccount : accounts) {
+            if (bankAccount.getAccountNumber().equals(accountNumber)) {
+                System.out.println(bankAccount);
+                return bankAccount;
+            }else{
+            return null;
+            }
+        }
+        return null;
+    }
+
+    public void deposit(String accountNumber, double amount){
         for (BankAccount bankAccount : accounts) {
             if (bankAccount.getAccountNumber().equals(accountNumber)) {
                 bankAccount.deposit(amount);
+            }else{
+
             }
         }
     }
 
-    public String withdrawFromAccount(String accountNumber, double amount) {
+    public String withdrawFromAccount(String accountNumber, double amount){
         for (BankAccount bankAccount : accounts) {
             if (bankAccount.getAccountNumber().equals(accountNumber)) {
-                if (bankAccount.getBalance() > amount) {
+                if(bankAccount.getBalance()>amount){
                     bankAccount.withdraw(amount);
                     return "Amount withdraw successfully";
-                } else {
+                }else{
                     return "Account's balance too low!";
                 }
             }
@@ -55,7 +71,7 @@ public class BankAccountManager {
         return "Account not found";
     }
 
-    public double getAccountBalance(String accountNumber) {
+    public double getAccountBalance(String accountNumber){
         for (BankAccount bankAccount : accounts) {
             if (bankAccount.getAccountNumber().equals(accountNumber)) {
                 return bankAccount.getBalance();
